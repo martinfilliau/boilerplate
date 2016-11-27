508ef0ce7ea074b0666c6628a6ffdb14807b3026
amaalej <amaalej@talend.com>
2016-11-27 03:11:44 +0100

    change selected pnr row css style

--- src/front/components/backoffice.dashboard/backoffice.dashboard.controller.js
+++ src/front/components/backoffice.dashboard/backoffice.dashboard.controller.js
@@ -5,23 +5,25 @@ export default class DashboardCtrl {
 	constructor($scope, $http, $timeout){
 		this.$timeout = $timeout;
 		this.$scope = $scope;
-		this.$scope.paxName = "maalej";
 		this.backofficeDashboardService = BackofficeDashboardService($http);
-		console.log(BackofficeDashboardService());
+		this.$scope.selectedPnr = null;
 
 		this.backofficeDashboardService.getAllPnrs()
 			.then((allPnrs)=>{
 				this.$scope.allPnrs = allPnrs;
+				this.$scope.filteredPnrs = allPnrs;
 			}, (err) => {
 				alert('Impossible de requeter la liste des PNRs');
 			});
 
-		this.$scope.showPnrDetails = (pnr) => {
-			console.log(pnr);
-			this.$timeout(() => {
-				this.$scope.selectedPnr = pnr;
-			});
-		}
+		this.$scope.showPnrDetails = (pnr, index) => {
+			this.$scope.selectedPnrIndex = index;
+			this.$scope.selectedPnr = pnr;
+		};
+
+		//setInterval(() => {
+		//	console.log(this.$scope.filteredPnrs)
+		//}, 2000);
 	}
 
 }
